689a2f7ab3f93a291bc4f6e45f879750694bafd4
Usman Rashid <usman1994rasheed@gmail.com>
2024-05-13 22:31:23 +0500

    barcode implemented

--- src/screens/GenerateQR.tsx
+++ src/screens/GenerateQR.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import colors from '../assets/colors';
 import QRCode from 'react-native-qrcode-svg';
-// import Barcode from 'react-native-barcode-builder';
+import Barcode from '@kichiyaki/react-native-barcode-generator';
+import ImagePath from '../Constants/ImagePath';
+import SettingsButton from '../Components/SettingsButton';
+import {useNavigation, useRoute, useIsFocused} from '@react-navigation/native';
+import {LABELS, getLabels} from '../labels';
 
 const GenerateQR = () => {
+  const route = useRoute();
+  const {params} = route;
+  const selectedLanguage = params ? params.selectedLanguage : 'english'; // Default to 'english' if selectedLanguage is not provided
+  let labels = getLabels(selectedLanguage);
+
+  const navigation = useNavigation(); // Use useNavigation hook to get navigation object
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    // Update labels when screen is focused
+    if (isFocused) {
+      labels = getLabels(selectedLanguage);
+    }
+  }, [isFocused, selectedLanguage]);
   return (
     <View style={styles.mainContainer}>
       <SafeAreaView />
-      <Text>GENERATE</Text>
+      <Text>{LABELS.touchToScan}</Text>
       <QRCode
         value="Just some string value"
-        backgroundColor="grey"
-        size={200}
+        // backgroundColor="grey"
+        size={250}
+        logo={ImagePath.settingsIcon}
+        logoSize={40}
+        logoBackgroundColor="white"
       />
-      {/* <Barcode value="Hello World" format="CODE128" />; */}
+      <Barcode
+        format="CODE128"
+        value="0123456789012"
+        text="0123456789012"
+        style={{marginBottom: 40, backgroundColor: 'red'}}
+        maxWidth={200}
+      />
+      <SettingsButton onPress={() => navigation.navigate('SettingsScreen')} />
     </View>
   );
 };
