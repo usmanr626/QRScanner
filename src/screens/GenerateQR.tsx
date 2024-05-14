11b0b0f799edce539a87b2027fa7434ee7a845de
Usman Rashid <usman1994rasheed@gmail.com>
2024-05-15 01:10:36 +0500

    minor logic implementation

--- src/screens/GenerateQR.tsx
+++ src/screens/GenerateQR.tsx
@@ -8,6 +8,7 @@ import {
   Button,
   TouchableOpacity,
   Image,
+  Modal,
 } from 'react-native';
 import colors from '../assets/colors';
 import QRCode from 'react-native-qrcode-svg';
@@ -18,6 +19,7 @@ import {useNavigation, useRoute, useIsFocused} from '@react-navigation/native';
 import {LABELS, getLabels} from '../labels';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {WIDTH} from '../assets/styles';
+import {Picker} from '@react-native-picker/picker';
 
 const GenerateQR = () => {
   const route = useRoute();
@@ -32,6 +34,9 @@ const GenerateQR = () => {
 
   const [userImage, setUserImage] = useState(null);
   const [inputText, setInputText] = useState('');
+
+  const [selectedFormat, setSelectedFormat] = useState(null);
+  const [formatPicker, setFormatPicker] = useState(false);
 
   useEffect(() => {
     // Update labels when screen is focused
@@ -51,6 +56,18 @@ const GenerateQR = () => {
   const handleInputChange = text => {
     console.log('🎯: GenerateQR -> text', text);
     setInputText(text);
+  };
+
+  const handleFormatChange = itemValue => {
+    setSelectedFormat(itemValue);
+    console.warn('done');
+    setFormatPicker(false);
+
+    // setModalVisible(false);
+  };
+
+  const errorHandler = () => {
+    console.log('Adasdas');
   };
 
   return (
@@ -91,7 +108,9 @@ const GenerateQR = () => {
         }}>
         {userImage === null ? (
           <TouchableOpacity
-            onPress={imageHandler}
+            onPress={() => {
+              selectedFormat !== null ? errorHandler() : imageHandler();
+            }}
             activeOpacity={0.8}
             style={styles.buttonStyle}>
             <Text numberOfLines={3} style={{textAlign: 'center'}}>
@@ -99,7 +118,9 @@ const GenerateQR = () => {
             </Text>
           </TouchableOpacity>
         ) : (
-          <TouchableOpacity onPress={imageHandler}>
+          <TouchableOpacity
+            onLongPress={() => setUserImage(null)}
+            onPress={imageHandler}>
             <Image
               source={{uri: userImage}}
               resizeMode="contain"
@@ -114,14 +135,17 @@ const GenerateQR = () => {
             />
           </TouchableOpacity>
         )}
-        <TouchableOpacity activeOpacity={0.8} style={styles.buttonStyle}>
+        <TouchableOpacity
+          onPress={() => (userImage ? errorHandler() : setFormatPicker(true))}
+          activeOpacity={0.8}
+          style={styles.buttonStyle}>
           <Text numberOfLines={3} style={{textAlign: 'center'}}>
-            {LABELS.selectFormat}
+            {selectedFormat ? selectedFormat : LABELS.selectFormat}
           </Text>
         </TouchableOpacity>
       </View>
 
-      <View style={{alignItems: 'center', top: 40}}>
+      <View style={{alignItems: 'center', top: '12%'}}>
         <TouchableOpacity
           onPress={() => console.log('asd', inputText)}
           style={{
@@ -145,6 +169,64 @@ const GenerateQR = () => {
         logoBackgroundColor="white"
         quietZone={5}
       /> */}
+
+      {/* <Barcode
+        format="CODE128"
+        value="0123456789012"
+        text="0123456789012"
+        style={{marginBottom: 40, backgroundColor: 'red'}}
+        maxWidth={200}
+      /> */}
+
+      {formatPicker && (
+        <Picker
+          selectedValue={selectedFormat}
+          style={styles.pickerStyle}
+          onValueChange={handleFormatChange}>
+          <Picker.Item label="CODE39" value="CODE39" />
+          <Picker.Item label="CODE128" value="CODE128" />
+          <Picker.Item label="CODE128A" value="CODE128A" />
+          <Picker.Item label="CODE128B" value="CODE128B" />
+          <Picker.Item label="CODE128C" value="CODE128C" />
+          <Picker.Item label="EAN13" value="EAN13" />
+          <Picker.Item label="EAN8" value="EAN8" />
+          <Picker.Item label="EAN5" value="EAN5" />
+          <Picker.Item label="EAN2" value="EAN2" />
+          <Picker.Item label="UPC" value="UPC" />
+          <Picker.Item label="UPCE" value="UPCE" />
+          <Picker.Item label="ITF14" value="ITF14" />
+          <Picker.Item label="ITF" value="ITF" />
+          <Picker.Item label="MSI" value="MSI" />
+          <Picker.Item label="MSI10" value="MSI10" />
+          <Picker.Item label="MSI11" value="MSI11" />
+          <Picker.Item label="MSI1010" value="MSI1010" />
+          <Picker.Item label="MSI1110" value="MSI1110" />
+          <Picker.Item label="pharmacode" value="pharmacode" />
+          <Picker.Item label="codabar" value="codabar" />
+          <Picker.Item label="Remove" value={null} />
+          {/* Add more Picker.Item components for additional formats */}
+        </Picker>
+      )}
+
+      {/* <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContent}>
+            <Picker
+              selectedValue={selectedFormat}
+              style={{height: 200, width: '100%'}}
+              onValueChange={handleFormatChange}>
+              <Picker.Item label="Format 1" value="format1" />
+              <Picker.Item label="Format 2" value="format2" />
+             
+             
+            </Picker>
+          </View>
+        </View>
+      </Modal> */}
     </View>
   );
 };
@@ -160,6 +242,11 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingHorizontal: 10,
   },
+  pickerStyle: {
+    height: 50,
+    width: WIDTH,
+    bottom: '10%',
+  },
   buttonStyle: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -168,5 +255,23 @@ const styles = StyleSheet.create({
     backgroundColor: colors.grey5,
     borderRadius: 20,
     padding: 5,
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+
+    // alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    width: WIDTH / 2,
+    height: 150,
+    position: 'absolute',
+    right: 20,
+    top: 100,
   },
 });
